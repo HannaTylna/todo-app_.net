9490674e4e1fb87f290f44d2a71224036a573a5c
HannaTylna <hanna.tylna@gmail.com>
2022-11-17 18:01:50 +0100

    fix cors

--- Program.cs
+++ Program.cs
@@ -1,20 +1,10 @@
-﻿
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using todo_app_.net.Models;
 using todo_app_.net.Services;
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("https://localhost:7007",
-                                              "https://localhost:44463");
-                      });
-});
+
 // Add services to the container.
 builder.Services.Configure<MongoBDSettings>(builder.Configuration.GetSection("Todo_dotnet"));
 builder.Services.AddSingleton<MongoDBService>();
@@ -22,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -32,9 +28,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseCors(MyAllowSpecificOrigins);
+//app cors
+app.UseCors("corsapp");
 
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
